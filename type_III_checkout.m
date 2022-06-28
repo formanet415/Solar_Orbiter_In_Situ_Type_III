b7faad94110e68934ccf77d0d4c7c4997d084bfa
Tom <58693041+formanet415@users.noreply.github.com>
2022-06-28 17:39:45 +0200

    Improved plotting and browsed most of 2021 data

--- type_III_checkout.m
+++ type_III_checkout.m
@@ -6,6 +6,7 @@ caa_data_paths;
 if ~exist('tlen','var') || isempty(tlen)
     tlen = 60*60*3;
 end
+%figure('Renderer', 'painters', 'Position', [50 50 1200 600])
 if date<datenum(2021,10,23)
     solo_panel_epd_step_rates_spectrum(date,tlen)
 else
@@ -18,7 +19,9 @@ folder = ['events' filesep sprintf('%i%i%i',y,m,d)];
 if ~exist(folder, 'dir')
     mkdir(folder)
 end
-saveas(gcf, [folder, filesep, sprintf('EPD_STEP_%s.png',datestr(date,'yyyymmdd_HHMMSS'))])
-saveas(gcf, ['events' filesep 'all', filesep, sprintf('EPD_STEP_%s.png',datestr(date,'yyyymmdd_HHMMSS'))])
+f=gcf;
+f.Position = [100 100 1800 700];
+exportgraphics(gcf, [folder, filesep, sprintf('EPD_STEP_%s.png',datestr(date,'yyyymmdd_HHMMSS'))],'Resolution','300')
+exportgraphics(gcf, ['events' filesep 'all', filesep, sprintf('EPD_STEP_%s.png',datestr(date,'yyyymmdd_HHMMSS'))],'Resolution','300')
 end
 
