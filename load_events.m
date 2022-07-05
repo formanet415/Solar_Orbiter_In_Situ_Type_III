f7d1c68c5e319241db945dc5bbdd3a9b90016d2b
Tom <58693041+formanet415@users.noreply.github.com>
2022-07-05 14:53:59 +0200

    Finished event loader

--- load_events.m
+++ load_events.m
@@ -6,7 +6,9 @@ if ~exist('fname','var') || isempty(fname)
     fname = ['events' filesep 'event_list.txt'];
 end
 
-T = readtable(fname);           
+opts = detectImportOptions(fname);
+opts = setvartype(opts, 'Var11', 'char');
+T = readtable(fname,opts);           
 ist3.year = table2array(T(:,1));             % year of event
 ist3.month = table2array(T(:,2));            % month of event
 ist3.day = table2array(T(:,3));              % day of beginning of event
@@ -16,8 +18,23 @@ ist3.epd_nxtday = table2array(T(:,6));       % does the energetic beam arrive on
 ist3.epd_hour = table2array(T(:,7));         % hour at which EPD STEP detects an increase in flux
 ist3.epd_minute = table2array(T(:,8));       % minute at which EPD STEP detects an increase in flux
 ist3.tswf_nxtday = table2array(T(:,9));      % are the triggered snapshots with langmuir waves on the next day
-ist3.tswf_indexes = table2array(T(:,10));    % indexes from the tswf cdf file which contain langmuir waves possibly caused by the energetic particles
-ist3.tswf_frequency = table2array(T(:,11));  % frequency/frequencies of langmuir waves from the triggered snapshots
+tindx = table2array(T(:,10));
+for i = 1:length(tindx)
+    linedata = char(strtrim(tindx(i)));
+    tmp = str2num([ '[' linedata ']']); %#ok<ST2NM>
+    idxs(i,1:length(tmp)) = tmp; %#ok<AGROW>
+end
+idxs(idxs == 0) = NaN;
+ist3.tswf_indexes = idxs;                    % indexes from the tswf cdf file which contain langmuir waves possibly caused by the energetic particles
+
+freqs = table2array(T(:,11));
+for i = 1:length(freqs)
+    linedata = char(strtrim(freqs(i)));
+    tmp = str2num([ '[' linedata ']']); %#ok<ST2NM>
+    fqs(i,1:length(tmp)) = tmp; %#ok<AGROW>
+end
+fqs(fqs == 0) = NaN;
+ist3.tswf_frequency = fqs;                   % frequency/frequencies of langmuir waves from the triggered snapshots
 ist3.langmuir_nxtday = table2array(T(:,12)); % do the langmuir waves show up on regular snapshots first on the next day
 ist3.langmuir_hour = table2array(T(:,13));   % hour when langmuir waves show up on regular snapshots
 ist3.langmuir_minute = table2array(T(:,14)); % minute when langmuir waves show up on regular snapshots
