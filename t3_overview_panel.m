de0c6a4cafc3ba734f8822111cefc1e091feb742
Tom <58693041+formanet415@users.noreply.github.com>
2022-07-09 09:50:42 +0200

    improved version of t3 panel plotter
    +added step main for later events
    +fixed colorbar placement
    +fixed issues with legend on the second subplot
    +added cone angle plot
    +added figure export
    included are two example plots

--- t3_overview_panel.m
+++ t3_overview_panel.m
@@ -2,6 +2,8 @@ function t3_overview_panel(year, month, day, h, m, epd_nxt, epd_h, epd_m, tswf_n
 %T3_OVERVIEW_PANEL Plotter function for individual events which analyses
 %them and saves panels with the data
 
+caa_data_paths;
+
 rswf = tdscdf_load_l2_surv_rswf(datetime(year,month,day));
 rtime0 = datenum(year, month, day, h, m, 0) - 1/48;
 rtime1 = rtime0 + 2/12 - 1/48;
@@ -21,7 +23,8 @@ for i = 1:length(ridxs)
     %bort = ([bpn(2),-bpn(1)]*srfuu);
     [sp, fq, nav] = make_spectrum(srfuu, length(srfuu(1,:))/8, 1/rswf.samp_rate(ridxs(i)),rswf.samp_rate(ridxs(i))/2);
     %[spz, fq, nav] = make_spectrum(srfuu(2,:), length(srfuu(2,:)), 1/rswf.samp_rate(ridxs(i)));
-    sps(i,:) = sum(squeeze(sp)');
+    temp = sum(squeeze(sp)');
+    sps(i,1:length(temp)) = temp;
     tt(i) = rswf.epoch(ridxs(i));
 end
 if r1 == length(rswf.epoch)
@@ -40,37 +43,64 @@ if r1 == length(rswf.epoch)
     disp('day overflow, skipping this for now')
 end
 
-subplot(3,1,1);
+subplot(4,1,1);
 opts.colorax=[-16,-9];
 hcbar =plot_spectrogram(sps',fq,tt,opts);
 xlim([rtime0,rtime1])
 title(sprintf('TDS RSWF spectrogram %s',datestr(rtime0,'yyyy-mm-dd HH:MM:SS.FFF')))
+cb=colorbar;
+cb.Position=[0.91,0.77,0.01,0.16];
 
-subplot(3,1,2);
+subplot(4,1,2);
+if rtime0<datenum(2021,10,22)
 solo_panel_epd_step_rates_spectrum(rtime0,4*60*60)
+elseif rtime0>datenum(2021,10,22)
+    solo_panel_epd_step_main_spectrum(rtime0,4*60*60)
+end
 xlim([rtime0,rtime1])
-colorbar
+
 
 [pastt,pasden] = caadb_get_solo_swa_pas_moments(rtime0,4*60*60);
-subplot(3,1,3)
+subplot(4,1,3)
+hold off
+
 plot(pastt,pasden,'r','DisplayName','PAS density')
-datetick()
-ylabel('Density [cm^-3]')
-xlim([rtime0,rtime1])
-hold on
+if ~isempty(pastt)
+    hold on
+end
 
 [biatt,biaden] = caadb_get_solo_rpw_bia_density(rtime0,4*60*60);
 plot(biatt,biaden,'g','DisplayName','BIAS density')
+if ~isempty(biatt)
+    hold on
+end
 
-title('Plasma density measured by SolO instruments')
 [tnrtt,tnrden]=caadb_get_solo_rpw_tnr_density(rtime0,4*60*60);
 plot(tnrtt,tnrden,'b','DisplayName','TNR density')
-legend()
+
+title('Plasma density measured by SolO instruments')
+datetick()
+ylabel('Density [cm^-3]')
+xlim([rtime0,rtime1])
+l = legend();
 
 
+subplot(4,1,4)
+[ep, b_vec, b_range, time_res, qf]=caadb_get_solo_mag(rtime0,60*60*4,'rtn');
+if ~isempty(ep)
+    coneang = rad2deg(acos(b_vec(1,:)./vecnorm(b_vec)));
+    plot(ep, coneang,'b','Displayname','RTN cone angle')
+    datetick()
+    xlim([rtime0,rtime1])
+    legend()
+    title('MAG RTN cone angle')
+else
+    title('no MAG data')
+end
 
-hold off
+
 disp('xd')
+exportgraphics(gcf, ['overview plots' filesep sprintf('TYPE_III_overview_panel_%s.png',datestr(rtime0,'yyyymmdd_HHMMSS'))],'Resolution','300')
 
 end
 
