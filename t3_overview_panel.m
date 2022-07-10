2974fe17093acf5a452fef0540f37b0a8dd0a03a
Tom <58693041+formanet415@users.noreply.github.com>
2022-07-10 09:37:11 +0200

    better comments and EPD jet time

--- t3_overview_panel.m
+++ t3_overview_panel.m
@@ -12,8 +12,10 @@ rtime1 = rtime0 + 2/12 - 1/48;
 
 ridxs = r0:r1;
 for i = 1:length(ridxs)
-    % add projection onto B field or at least convert to SRF
+    
     srfuu = convert_to_SRF(rswf,ridxs(i));  % This takes samps_per_ch into account
+    
+    % commented an example of conversion to B paralell and B perpendicular
     %srfuu(1:2,:) = rswf.data(1:2,:,i);
     %[ep, b_vec, b_range, time_res, qf]=caadb_get_solo_mag(datenum(year,month,day),1,'srf');
     %b = b_vec(:,1);
@@ -21,15 +23,18 @@ for i = 1:length(ridxs)
     %bpn = bp/sqrt(bp'*bp);
     %bper = (srfuu'*bpn)';
     %bort = ([bpn(2),-bpn(1)]*srfuu);
-    [sp, fq, nav] = make_spectrum(srfuu, length(srfuu(1,:))/8, 1/rswf.samp_rate(ridxs(i)),rswf.samp_rate(ridxs(i))/2);
     %[spz, fq, nav] = make_spectrum(srfuu(2,:), length(srfuu(2,:)), 1/rswf.samp_rate(ridxs(i)));
+    
+    [sp, fq, nav] = make_spectrum(srfuu, length(srfuu(1,:))/8, 1/rswf.samp_rate(ridxs(i)),rswf.samp_rate(ridxs(i))/2);
+    
     temp = sum(squeeze(sp)');
-    sps(i,1:length(temp)) = temp;
+    sps(i,1:length(temp)) = temp; % handles changes in samp rate
     tt(i) = rswf.epoch(ridxs(i));
 end
 if r1 == length(rswf.epoch)
     li=i;
-    %tbd load next day and append some stuff
+    % loading next day if nescessary
+    disp('loading next day')
     rswf2 = tdscdf_load_l2_surv_rswf(datetime(year,month,day)+1);
     r2 = 1;
     [~, r3] = min(abs(rswf2.epoch - rtime1));
@@ -40,18 +45,24 @@ if r1 == length(rswf.epoch)
         sps(li+i,:) = sum(squeeze(sp)');
         tt(li+i) = rswf2.epoch(ridxs2(i));
     end
-    disp('day overflow, skipping this for now')
 end
 
-subplot(4,1,1);
-opts.colorax=[-16,-9];
-hcbar =plot_spectrogram(sps',fq,tt,opts);
+nsplts = 5; %number of subplots
+
+
+
+
+subplot(nsplts,1,1);    % RSWF spectrogram from L2 data
+opts.colorax = [-16,-9];
+hcbar = plot_spectrogram(sps',fq,tt,opts);
 xlim([rtime0,rtime1])
 title(sprintf('TDS RSWF spectrogram %s',datestr(rtime0,'yyyy-mm-dd HH:MM:SS.FFF')))
 cb=colorbar;
 cb.Position=[0.91,0.77,0.01,0.16];
+xline(datenum(year,month,day+epd_nxt,epd_h,epd_m,0));
 
-subplot(4,1,2);
+
+subplot(nsplts,1,2);    % EPD STEP panel
 if rtime0<datenum(2021,10,22)
 solo_panel_epd_step_rates_spectrum(rtime0,4*60*60)
 elseif rtime0>datenum(2021,10,22)
@@ -59,9 +70,11 @@ elseif rtime0>datenum(2021,10,22)
 end
 xlim([rtime0,rtime1])
 
-
 [pastt,pasden] = caadb_get_solo_swa_pas_moments(rtime0,4*60*60);
-subplot(4,1,3)
+xline(datenum(year,month,day+epd_nxt,epd_h,epd_m,0));
+
+
+subplot(nsplts,1,3)     % Plasma density
 hold off
 
 plot(pastt,pasden,'r','DisplayName','PAS density')
@@ -82,25 +95,32 @@ title('Plasma density measured by SolO instruments')
 datetick()
 ylabel('Density [cm^-3]')
 xlim([rtime0,rtime1])
-l = legend();
+l = legend('AutoUpdate','off');
+xline(datenum(year,month,day+epd_nxt,epd_h,epd_m,0));
 
 
-subplot(4,1,4)
+subplot(nsplts,1,4)     % Magnetic field cone angle (tbd more about B field)
 [ep, b_vec, b_range, time_res, qf]=caadb_get_solo_mag(rtime0,60*60*4,'rtn');
 if ~isempty(ep)
     coneang = rad2deg(acos(b_vec(1,:)./vecnorm(b_vec)));
     plot(ep, coneang,'b','Displayname','RTN cone angle')
     datetick()
     xlim([rtime0,rtime1])
-    legend()
+    ylabel('cone angle [deg]')
+    legend('AutoUpdate','off')
     title('MAG RTN cone angle')
 else
     title('no MAG data')
+    xlim([rtime0,rtime1])
 end
+xline(datenum(year,month,day+epd_nxt,epd_h,epd_m,0));
 
+
+subplot(nsplts,1,5)     % Statistics from TSWF 
+% TBD
 
 f = gcf;
-f.Position = [100 100 1700 1000];
+f.Position = [100 100 1700 1300];
 saveas(f, ['overview plots' filesep sprintf('TYPE_III_overview_panel_%s.png',datestr(rtime0,'yyyymmdd_HHMMSS'))])
 %exportgraphics(f, ['overview plots' filesep sprintf('TYPE_III_overview_panel_%s.png',datestr(rtime0,'yyyymmdd_HHMMSS'))],'Resolution','300')
 
